c6b9d5c74fb29d99924ac89aa5744603072dab5d
Haojun Yang <xiaoxin88420@hotmail.com>
2020-07-30 22:26:09 -0700

    make changes

--- app.js
+++ app.js
@@ -2,43 +2,89 @@
 let timenow = moment().format('llll')
 document.getElementById('currentDay').append(timenow)
 
-//capture the current hour from moment.js
-let currentHour = parseInt(moment().format('H'))
-$(document).ready( function (){
+$(document).ready(function (){
 
-    $('.saveBtn').click(function(){
-        let value = $(this).siblings('.description').val()
-        let time = $(this).parent().attr('id')
-        localStorage.setItem(time,value)
+    $(".saveBtn").click(function() {
+        let value = $(this).siblings(".description").val()
+        console.log(value)
+        let time = $(this).parents().attr("id")
+        console.log(time)
+
+        localStorage.setItem(time, value)
+
+    for (let i=9; i<18; i++){
+        $('#${i}.description').val(localStorage.getItem(i))
+    }        
+
+    // $("#9 .description").val(localStorage.getItem("9"))
+    // $("#10 .description").val(localStorage.getItem("10"))
+    // $("#11 .description").val(localStorage.getItem("11"))
+    // $("#12 .description").val(localStorage.getItem("12"))
+    // $("#13 .description").val(localStorage.getItem("13"))
+    // $("#14 .description").val(localStorage.getItem("14"))
+    // $("#15 .description").val(localStorage.getItem("15"))
+    // $("#16 .description").val(localStorage.getItem("16"))
+    // $("#17 .description").val(localStorage.getItem("17"))
+
     })
-    
-    // for (let i=9; i<18; i++){
-    //     $('#i.description').val(localStorage.getItem('i'))
-    // }
-    //Unfortunately the upper sentence doesn't work...so I have to use a dummy method...
-    $("#9 .description").val(localStorage.getItem("9"))
-    $("#10 .description").val(localStorage.getItem("10"))
-    $("#11 .description").val(localStorage.getItem("11"))
-    $("#12 .description").val(localStorage.getItem("12"))
-    $("#13 .description").val(localStorage.getItem("13"))
-    $("#14 .description").val(localStorage.getItem("14"))
-    $("#15 .description").val(localStorage.getItem("15"))
-    $("#16 .description").val(localStorage.getItem("16"))
-    $("#17 .description").val(localStorage.getItem("17"))
 
-    //set current hour color to red and so on
-    $('.time-block').each(function(){
-        let color = parseInt($this).attr('id')
-            if(color === currentHour) {
-                $(this).addClass('present')
-            }   else if (color < currentHour) {
-                $(this).addClass('past')
-            }   else {
-                $(this).addClass('future')
-            }
-    })  
+
+    let currentHour = parseInt(moment().format("H"))
+
+    $(".time-block").each(function () {
+       let timeBlock = parseInt($(this).attr("id"))
+        if(timeBlock === currentHour){
+            $(this).addClass("present")
+        } else if (timeBlock > currentHour) {
+            $(this).addClass("future")
+        } else {
+            $(this).addClass("past")
+        }
+
+    })
+
 
 })
+
+
+
+//capture the current hour from moment.js
+// let currentHour = parseInt(moment().format('H'))
+// $(document).ready( function (){
+
+//     $('.saveBtn').click(function(){
+//         let value = $(this).siblings('.description').val()
+//         console.log(value)
+//         let time = $(this).parents().attr('id')
+//         console.log(time)
+//         localStorage.setItem(time,value)
+//     })
+    
+
+    //If the upper sentence doesn't work... I'll use a dummy method...
+    // $("#9 .description").val(localStorage.getItem("9"))
+    // $("#10 .description").val(localStorage.getItem("10"))
+    // $("#11 .description").val(localStorage.getItem("11"))
+    // $("#12 .description").val(localStorage.getItem("12"))
+    // $("#13 .description").val(localStorage.getItem("13"))
+    // $("#14 .description").val(localStorage.getItem("14"))
+    // $("#15 .description").val(localStorage.getItem("15"))
+    // $("#16 .description").val(localStorage.getItem("16"))
+    // $("#17 .description").val(localStorage.getItem("17"))
+
+    //set current hour color to red and so on
+//     $('.time-block').each(function() {
+//         let color = parseInt($(this).attr('id'))
+//             if(color === currentHour) {
+//                 $(this).addClass('present')
+//             }   else if (color < currentHour) {
+//                 $(this).addClass('past')
+//             }   else {
+//                 $(this).addClass('future')
+//             }
+//     })  
+
+// })
 
 
 
@@ -61,3 +107,5 @@ $(document).ready( function (){
 //         addClass.classList.add('future')
 //     }
 // }
+
+
